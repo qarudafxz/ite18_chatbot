fd93d2721421e3808bb440961cd79f2a36db7c49
qaruda <70809588+francistinao@users.noreply.github.com>
2023-04-16 01:59:01 +0800

    update

---
+++ data/Questions.js
@@ -0,0 +1,43 @@
+const creatorQuestions = [
+	"Who made you",
+	"who made you",
+	"who is your creator",
+	"creator",
+	"who created you",
+	"who built you",
+	"Who built you",
+	"May I ask if who built you",
+	"May I ask if who made you",
+	"developer",
+	"Who is your developer",
+	"developed",
+	"develops",
+	"Who created you",
+	"Who is your creator",
+];
+
+const loverQuestions = [
+	"Who is the creator's love",
+	"Who is the creator's love of his life",
+	"Who is your developer's love of his life",
+	"Who is your developer's love",
+	"Who is your developer's lover",
+	"What is the name of your developer's girlfriend",
+	"Who is the best girlfriend of your developer",
+	"Who is the best girlfreind in the world",
+	"love",
+	"lover",
+	"developer's love",
+	"creator's love",
+	"creator's love of his life",
+	"developer's love of his life",
+	"developer's girlfriend",
+	"developer's lover",
+	"developer's best girlfriend",
+	"creator's best girlfriend",
+	"What is the name of your developer's girlfriend?",
+	"what is the name of the girlfriend the one who wrote you?",
+];
+
+export { loverQuestions as LoveQuestions };
+export { creatorQuestions as Questions };
